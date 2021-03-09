898ac218913ffc4f9d6a89554864730e39c1613e
TheNlesh <60070062@kmitl.ac.th>
2021-03-09 17:49:18 +0700

    Added task1 create loopback for all device

--- Week4/HW4_Netmiko.py
+++ Week4/HW4_Netmiko.py
@@ -1,6 +1,7 @@
 from netmiko import NetMikoAuthenticationException, NetmikoAuthError, NetmikoTimeoutError, NetMikoTimeoutException
 from netmiko import ConnectHandler
 import getpass
+import threading
 
 class Manager:
     def __init__(self, ip, username, password, device_type):
@@ -80,20 +81,114 @@ class Manager:
         finally:
             connection.disconnect()
 
+    def show_hostname(self):
+        connection = self.create_connection()
+        if not connection:
+            return False
+        try:
+            hostname = connection.send_command("sh run | in hostname")
+        except Exception as e:
+            return False
+        else:
+            return hostname.split()[1]
+        finally:
+            connection.disconnect()
+
+    def create_vrf(self, vrf_name, as_number, vrf_number):
+        connection = self.create_connection()
+        if not connection:
+            return False
+        try:
+            vrf_cmd = ["conf t", "ip vrf {}".format(vrf_name), "rd {}:{}".format(str(as_number), str(vrf_number)), "end"]
+            for cmd in vrf_cmd:
+                connection.send_command(cmd, expect_string=r"#")
+
+        except Exception as e:
+            return False
+        else:
+            return True
+        finally:
+            connection.disconnect()
+
+    def show_vrf(self):
+        connection = self.create_connection()
+        if not connection:
+            return False
+
+        try:
+            vrf_list = connection.send_command("sh vrf", expect_string=r"#")
+        except Exception as e:
+            return False
+        else:
+            return vrf_list
+        finally:
+            connection.disconnect()
+
+    def create_loopback(self, number, ip, mask, vrf_name):
+        """ Create loopback from loopback number, ip, and subnet mask """
+        connection = self.create_connection()
+        if not connection:
+            return False
+        try:
+            loopbackcmd = ["conf t", "int lo{}".format(str(number)), "ip vrf forward {}".format(vrf_name), "ip add {} {}".format(ip, mask), "end"]
+            for cmd in loopbackcmd:
+                connection.send_command(cmd, expect_string=r"#")
+        except Exception as e:
+            return False
+        else:
+            return True
+        finally:
+            connection.disconnect()
+
+    def show_interface(self):
+        connection = self.create_connection()
+        if not connection:
+            return False
+
+        try:
+            interfaces = connection.send_command("sh ip int br", expect_string=r"#")
+        except Exception as e:
+            return False
+        else:
+            return interfaces
+        finally:
+            connection.disconnect()
+
+def task_1(host_template, loopback_template, last_octet, username, password, device_type):
+    """ Create loopback for all device """
+    host = host_template + str(last_octet)
+    loopback_ip = loopback_template + str(last_octet)
+
+    manageObj = Manager(ip=host, username=username, password=password, device_type=device_type)
+    manageObj.create_vrf(vrf_name="Net", as_number=300, vrf_number=100)
+    manageObj.create_loopback(number=100, ip=loopback_ip, mask="255.255.255.0", vrf_name="Net")
+
+    hostname = manageObj.show_hostname()
+    interfaces = manageObj.show_interface()
+    print(hostname, interfaces, sep="\n")
+    print("-"*50)
+
 
 if __name__ == '__main__':
     # Test method
-    host = "172.31.179.1"
+    host_template = "172.31.179."
+    loopback_template = "172.20.179."
     username = input("Username: ")
     password = getpass.getpass()
     device_type = "cisco_ios"
 
-    manageObj = Manager(host, username, password, device_type)
-    print(manageObj.backup_config(filename="OLDCONFIG"))
-    print(manageObj.save_config())
-    print(manageObj.rollback_config(interval=5))
+    # print(manageObj.backup_config(filename="OLDCONFIG"))
+    # print(manageObj.save_config())
+    # print(manageObj.rollback_config(interval=5))
 
-
+    # Task1 add loopback
+    task1_threads = []
+    for number in range(1, 10):
+        thread_arg = [host_template, loopback_template, number, username, password, device_type]
+        task1_threads.append(threading.Thread(target=task_1, args=thread_arg))
+        # task_1(host_template, loopback_template, number, username, password, device_type)
+    for t in task1_threads:
+        t.start()
 
 
 
